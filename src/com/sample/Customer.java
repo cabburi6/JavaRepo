8b2eb9bb9350fdb11ebca68d2a6640caf741a98c
ABBURI\ABBURI'S <cabburi6@gmail.com>
2016-10-08 19:42:45 -0400

    Fixed three defects

--- src/com/sample/Customer.java
+++ src/com/sample/Customer.java
@@ -2,6 +2,6 @@ package com.sample;
 
 public class Customer{
 	    String name;
-	    int phone;
+	    String phone;
 	    String email;
 	}
--- src/com/sample/Hello.java
+++ src/com/sample/Hello.java
@@ -3,12 +3,10 @@ package com.sample;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.*;
 
 public class Hello {
 
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 	    String name;
