439494999148b636a88bc15478d7b9f882ee1be0
ABBURI\ABBURI'S <cabburi6@gmail.com>
2016-10-11 21:07:59 -0400

    Added database programs and refactored Hello as CustomerDetails

--- src/com/sample/CustomerDetails.java
+++ src/com/sample/CustomerDetails.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Hello {
+public class CustomerDetails {
 
 
 	public static void main(String[] args) throws IOException {
